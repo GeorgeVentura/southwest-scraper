ac0032d2fcb285c208480cacd96f32f00ce74302
george <g.ventura18@gmail.com>
2016-10-01 01:05:20 -0400

    rebuilt to work with mobile API instead of web html.

--- scraper.py
+++ scraper.py
@@ -1,42 +1,103 @@
-from datetime import datetime, timedelta as td
-from request_page import RequestPage
-from parse_page import ParsePage
+'''
+    Author: George Ventura
+    Date:   9/30/2016
+
+    http://github.com/GeorgeVentura/southwest-scraper
+
+'''
+
+import requests
+import json
+import datetime
 
 
-class Scraper(object):
+class SouthWestRequest(object):
 
-    def __init__(self, origin_airports, destination_airports, start_date, end_date):
-        self.origin_airports = origin_airports
-        self.destination_airports = destination_airports
-        self.start_date = start_date
-        self.end_date = end_date
-        self.date_range = self.get_date_range()
+    def __init__(self, depart_date, destination_airport, origin_airport):
+        self.API_KEY = 'l7xx8d8bfce4ee874269bedc02832674129a'
+        self.headers = {'X-API-Key': 'l7xx8d8bfce4ee874269bedc02832674129b',
+                        'User-Agent': 'Southwest/3.6.16 (iPhone; iOS 9.3.2; Scale/2.00)'}
 
-    def get_date_range(self):
-        start_date_obj = datetime.strptime(self.start_date, '%m/%d/%Y').date()
-        end_date_obj = datetime.strptime(self.end_date, '%m/%d/%Y').date()
+        self.URL = 'https://mobile.southwest.com/api/extensions/v1/mobile/flights/products?currency-type=Dollars&departure-date={0}&destination-airport={1}&number-adult-passengers=1&number-senior-passengers=0&origination-airport={2}&promo-code='.format(depart_date, destination_airport, origin_airport)
+        self.flights = self.parse_response(self.make_request())
 
-        delta = end_date_obj - start_date_obj
-        date_range = []
-        for i in range(delta.days + 1):
-            date_range.append(start_date_obj + td(days=i))
-        return date_range
+    def make_request(self):
+        session = requests.Session()
+        session.headers = self.headers
+        try:
+            req = session.get(self.URL)
+            if req.status_code == 200:
+                json_response = json.loads(req.content)
+                return json_response
+            else:
+                print "Status Code http: " + req.status_code
+        except Exception as e:
+            print e
 
-    def start(self):
-        for origin_airport in self.origin_airports:
-            for destination_airport in self.destination_airports:
-                for depart_date in self.date_range:
-                        req = RequestPage(origin_airport,
-                                          destination_airport,
-                                          depart_date)
-                        parse = ParsePage(req.request_page())
-                        print parse.get_flight_data()
+    def parse_response(self, reps):
+        if 'message' in reps:
+            return 0
+        flights = []
+        if len(reps['trips']) == 0:
+            return 0
+        dat = reps['trips'][0]['airProducts']
+        for flight in dat:
+            flights.append(self.extract_data(flight))
+        return flights
 
+    def extract_data(self, flight):
+        record = dict()
+        record['flights'] = self.get_flights(flight['segments'])
+        record['duration'] = flight['durationMinutes']
+        record['price'] = flight['fareProducts'][2]['currencyPrice']['totalFareCents']
+        record['seats'] = flight['fareProducts'][2]['seatsAvailable']
+        datetime_retrieved = datetime.datetime.now()
+        record['datetime_retrieved'] = datetime_retrieved
+        record['num_connections'] = len(flight['segments'])
+        if len(flight['segments']) > 1:
+            connect_flight = self.get_connecting_flight(flight)
+            record['connect'] = connect_flight[0]
+            record['connect_depart_date'] = connect_flight[1]
+        else:
+            record['connect'] = None
+            record['connect_depart_date'] = None
+        return record
 
-originx_airports = ['PHX']
-dest_airports = ['LAX']
+    def get_connecting_flight(self, flight):
+        if len(flight['segments']) > 1:
+            flight_number_connect = flight['segments'][1]['operatingCarrierInfo']['flightNumber']
+            depart_datetime = flight['segments'][1]['departureDateTime']
+            return flight_number_connect, depart_datetime
 
-scrap = Scraper(originx_airports, dest_airports, '08/01/2016', '08/05/2016')
-scrap.start()
+    def get_flights(self, flights):
+        flight_data = []
+        for flight in flights:
+            flight_data.append({'depart_datetime': flight['departureDateTime'],
+                                'arrival_datetime': flight['arrivalDateTime'],
+                                'dest_airport': flight['destinationAirportCode'],
+                                'origin_airport': flight['originationAirportCode'],
+                                'num_stops': flight['numberOfStops'],
+                                'flight_number': flight['operatingCarrierInfo']['flightNumber'],
+                                'carrier_code': flight['operatingCarrierInfo']['carrierCode']})
+        return flight_data
 
+    def get_results(self):
+        return self.flights
 
+    def get_results_json(self):
+        return json.dumps(self.flights, default=SouthWestRequest.default)
+
+    @staticmethod
+    def default(obj):
+        """Default JSON serializer."""
+        import calendar, datetime
+
+        if isinstance(obj, datetime.datetime):
+            if obj.utcoffset() is not None:
+                obj = obj - obj.utcoffset()
+            millis = int(
+                calendar.timegm(obj.timetuple()) * 1000 +
+                obj.microsecond / 1000
+            )
+            return millis
+        raise TypeError('Not sure how to serialize %s' % (obj,))
